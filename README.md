26c6eb38ae79fac9969f22e87349829c66855e56
Frank Taubert <frank.taubert@uni-jena.de>
2019-01-10 10:40:27 +0100

    Initial ommit

---
+++ README.md
@@ -0,0 +1,6 @@
+# delatex README
+
+This is the README for your extension "delatex" for Visual Studio Code. 
+It will remove and replace Tex commands from Text.
+
+Most commands will be simply removed. Others, such as \caption{} or \ac{} will be replaced with their content (\ac{FOO} -> FOO)
---
+++ src/extension.ts
@@ -0,0 +1,76 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+
+function completeCommandRegex(commandName:string)
+{
+	var re = new RegExp( ""+commandName+"\\{([\\w\\d,:]*)\\}" , "g");		
+	return re;
+}
+
+function completeOptionalParameterRegex()
+{	
+	var regex = /\[[\w\d.\\=]*\]/g;
+	return regex ;		
+}
+
+function cleanup(toModify:string) {	
+	let result = toModify.replace(completeOptionalParameterRegex(),"");
+
+	let dropList = [ "cite" ,"autoref" , "label" , "chapter"];
+	for (let entry of dropList) {
+		let regEx = completeCommandRegex(entry);
+		result = result.replace(regEx,"");
+	}
+
+	let keepContentList = [ "ac" ,"paragraph", "section" , "subsection", "gls" , "acp", "caption"];
+	for (let entry of keepContentList) {
+		let regEx = completeCommandRegex(entry);
+		result = result.replace(regEx,"$1");
+	}	
+
+	return result.replace("()","");
+}
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "delatex" is now active!');
+
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with registerCommand
+	// The commandId parameter must match the command field in package.json
+	let disposable = vscode.commands.registerCommand('extension.DeLatex', () => {
+		// The code you place here will be executed every time your command is executed
+		
+		
+		let editor = vscode.window.activeTextEditor;
+		if (editor)
+		{
+
+			let doc = editor.document;
+			if (doc)
+			{
+				let fullText = doc.getText()								
+				const fullRange = new vscode.Range(
+    					doc.positionAt(0),
+						doc.positionAt(fullText.length ));
+						
+				let modif = cleanup(fullText);
+				editor.edit( (builder) => {			
+							 	builder.replace(fullRange,modif);
+							});				
+			}
+		}
+		// Display a message box to the user
+
+		vscode.window.showInformationMessage('Remove Latex Command Finished');
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
